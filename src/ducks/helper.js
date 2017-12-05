b35419364bc6e2440e01535061bddeba95abab6d
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2017-12-05 22:41:08 +0100

    Missing last on last commit

---
+++ src/ducks/helper.js
@@ -0,0 +1,56 @@
+/**
+ * GetRandomNumber
+ * 
+ * @description
+ * Generate random number
+ */
+export const getRandomNumber = (min = 1, max = 100) => Math.floor((Math.random() * max) + min);
+
+/**
+ * GetRandomOperator
+ * 
+ * @description
+ * Generate random operator
+ */
+export const getRandomOperator = () => {
+  const operators = ['+', '/', '*', '-'];
+
+  return operators[Math.floor((Math.random() * 3) + 0)];
+}
+
+/**
+ * Return Calcul result
+ * 
+ * @param {string} calcul
+ * 
+ * @returns {string}
+ */
+export const returnCalculResult = (calcul) => eval(calcul).toString();
+
+/**
+ * GenerateRandomOperation
+ * 
+ * @description
+ * Generate random string to calculate
+ */
+export const generateRandomOperation = () => {
+  const numberOfDigits = getRandomNumber(1, 5);
+
+  const array = Array.from(new Array(numberOfDigits));
+  const calcul = array
+    .reduce((acc, v, index) => {
+      const randomNumber = getRandomNumber(1, 30);
+      const randomOperator = getRandomOperator();
+
+      if (index === array.length - 1) {
+        return `${acc}${randomNumber}`;
+      }
+
+      return `${acc}${randomNumber}${randomOperator}`;
+    }, ``);
+
+  return {
+    calcul,
+    result: returnCalculResult(calcul),
+  };
+}
